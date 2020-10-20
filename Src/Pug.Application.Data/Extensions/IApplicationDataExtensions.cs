77e5848aaa13aeb8107a5b991e37202ac8cf53fd
Andrian <Andrian@ND.id.au>
2020-10-20 21:48:01 +1100

    Added async IApplicationData extension methods.

--- Src/Pug.Application.Data/Extensions/IApplicationDataExtensions.cs
+++ Src/Pug.Application.Data/Extensions/IApplicationDataExtensions.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 #if NETFX || NETSTANDARD_2_0
 using System.Transactions;
 #endif
@@ -131,6 +131,112 @@ namespace Pug.Application.Data
 			}
 		}
 
+		/// <summary>
+		/// This is a wrapper function that allows developer to perform data tasks without having to worry about 
+		/// </summary>
+		/// <param name="action">Action to perform when a new instance of T data session has been successfully created.</param>
+		/// <param name="transactionScopeOption">Specifies requirement and the scope of transaction.</param>
+		/// <param name="onError">Action to perform when an error occured prior to completion of <paramref name="action"/>, this includes when error occured during creation of T data session instance.</param>
+		/// <param name="errorHandler">Specifies if and how an error is to be handled. Corresponding error would be thrown by the function if this parameter is null.</param>
+		/// <param name="onSuccess">Action to perform upon successful completion of <paramref name="action"/>. Created instance of T data session would have already been disposed at this stage.</param>
+		/// <param name="onFinished">Action to perform upon completion of <paramref name="action"/> regardless of whether it was successful.</param>
+		public static async Task PerformAsync<TDataSession, TContext>(
+			this IApplicationData<TDataSession> applicationData, 
+			Func<TDataSession, TContext, Task> action, 
+			TContext context,
+#if NETFX || NETSTANDARD_2_0
+			TransactionScopeOption transactionScopeOption,
+			TransactionOptions transactionOptions,
+#else
+			bool transactionRequired = true, 
+#endif
+			Action<Exception, TContext> onError = null,
+			Action<Exception, TContext> errorHandler = null, 
+			Action<TContext> onSuccess = null, 
+			Action<TContext> onFinished = null
+		)		
+			where TDataSession : class, IApplicationDataSession
+		{
+			TDataSession dataSession = null;
+			bool successful = true;
+
+			try
+			{
+#if NETFX || NETSTANDARD_2_0
+
+				if(transactionScopeOption != TransactionScopeOption.RequiresNew && Transaction.Current != null &&
+					Transaction.Current.IsolationLevel == transactionOptions.IsolationLevel)
+				{
+					using( dataSession = applicationData.GetSession() )
+					{
+						await action(dataSession, context).ConfigureAwait(false);
+					}
+				}
+				else
+				{
+					using(TransactionScope tx = new TransactionScope(transactionScopeOption, transactionOptions))
+					{
+						dataSession = applicationData.GetSession();
+
+						await action(dataSession, context).ConfigureAwait(false);
+
+						tx.Complete();
+					}
+				}
+#else
+				dataSession = applicationData.GetSession();
+
+				if (transactionRequired)
+				{
+					dataSession.BeginTransaction();
+
+					await action(dataSession, context).ConfigureAwait(false);
+				
+					dataSession.CommitTransaction();
+				}
+				else
+				{
+					await action(dataSession, context).ConfigureAwait(false);
+				}
+#endif
+			}
+			catch (Exception exception)
+			{
+#if !(NETFX || NETSTANDARD_2_0)
+				if (transactionRequired)
+					dataSession.RollbackTransaction();
+#endif
+
+				successful = false;
+
+				if (onError != null)
+					onError(exception, context);
+
+				if (errorHandler == null)
+					throw;
+
+				errorHandler(exception, context);
+			}
+			finally
+			{
+				if (dataSession != null)
+					try
+					{
+						dataSession.Dispose();
+					}
+					catch (Exception finalException)
+					{
+						// todo: log error?
+					}
+
+				if (successful && onSuccess != null)
+					onSuccess(context);
+
+				if (onFinished != null)
+					onFinished(context);
+			}
+		}
+
 #if NETFX || NETSTANDARD_2_0
 		/// <summary>
 		/// This is a wrapper function that allows developer to perform data tasks without having to worry about 
@@ -163,6 +269,64 @@ namespace Pug.Application.Data
 					dataSession = applicationData.GetSession();
 
 					action(dataSession, context);
+
+					tx.Complete();
+				}
+			}
+			catch (Exception exception)
+			{
+				successful = false;
+
+				if (onError != null)
+					onError(exception, context);
+
+				if (errorHandler == null)
+					throw;
+
+				errorHandler(exception, context);
+			}
+			finally
+			{
+				if (dataSession != null)
+					try
+					{
+						dataSession.Dispose();
+					}
+					catch (Exception finalException)
+					{
+						// todo: log error?
+					}
+
+				if (successful && onSuccess != null)
+					onSuccess(context);
+
+				if (onFinished != null)
+					onFinished(context);
+			}
+		}
+		
+		public static async Task PerformAsync<TDataSession, TContext>(
+			this IApplicationData<TDataSession> applicationData, 
+			Func<TDataSession, TContext, Task> action, 
+			TContext context,
+			TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+			Action<Exception, TContext> onError = null,
+			Action<Exception, TContext> errorHandler = null, 
+			Action<TContext> onSuccess = null, 
+			Action<TContext> onFinished = null
+		)		
+			where TDataSession : class, IApplicationDataSession
+		{
+			TDataSession dataSession = null;
+			bool successful = true;
+
+			try
+			{
+				using (TransactionScope tx = new TransactionScope(transactionScopeOption))
+				{
+					dataSession = applicationData.GetSession();
+
+					await action(dataSession, context).ConfigureAwait(false);
 
 					tx.Complete();
 				}
@@ -291,6 +455,97 @@ namespace Pug.Application.Data
 			return result;
 		}
 
+		public static async Task<TResult> ExecuteAsync<TDataSession, TContext, TResult>(
+			this IApplicationData<TDataSession> applicationData, 
+			Func<TDataSession, TContext, Task<TResult>> function, 
+			TContext context,
+#if NETFX || NETSTANDARD_2_0
+			TransactionScopeOption transactionScopeOption,
+			TransactionOptions transactionOptions,
+#else
+			bool transactionRequired = true, 
+#endif
+			Action<Exception, TContext> onError = null, 
+			Action<Exception, TContext> errorHandler = null, 
+			Action<TContext> onSuccess = null, 
+			Action<TContext> onFinished = null
+		)
+			where TDataSession : class, IApplicationDataSession
+		{
+			TDataSession dataSession = null;
+			bool successful = true;
+			TResult result = default(TResult);
+
+			try
+			{
+#if NETFX || NETSTANDARD_2_0
+
+				if(transactionScopeOption != TransactionScopeOption.RequiresNew && Transaction.Current != null &&
+					Transaction.Current.IsolationLevel == transactionOptions.IsolationLevel)
+				{
+					using( dataSession = applicationData.GetSession() )
+					{
+						result = await function(dataSession, context).ConfigureAwait(false);
+					}
+				}
+				else
+				{
+					using(TransactionScope tx = new TransactionScope(transactionScopeOption, transactionOptions))
+					{
+						using(dataSession = applicationData.GetSession())
+						{
+							result = await function(dataSession, context).ConfigureAwait(false);
+						}
+
+						tx.Complete();
+					}
+				}
+#else
+				using(dataSession = applicationData.GetSession())
+				{
+					if(transactionRequired)
+					{
+						dataSession.BeginTransaction();
+
+						result = await function(dataSession, context).ConfigureAwait(false);
+
+						dataSession.CommitTransaction();
+					}
+					else
+					{
+						result = await function(dataSession, context).ConfigureAwait(false);
+					}
+				}
+#endif
+			}
+			catch (Exception exception)
+			{
+#if !(NETFX || NETSTANDARD_2_0)
+				if (transactionRequired)
+					dataSession.RollbackTransaction();
+#endif
+				successful = false;
+
+				if (onError != null)
+					onError(exception, context);
+
+				if (errorHandler == null)
+					throw;
+
+				errorHandler(exception, context);
+			}
+			finally
+			{
+				if (successful && onSuccess != null)
+					onSuccess(context);
+
+				if (onFinished != null)
+					onFinished(context);
+			}
+
+			return result;
+		}
+
 #if NETFX || NETSTANDARD_2_0
 		public static R Execute<T, C, R>(
 			this IApplicationData<T> applicationData,
@@ -315,6 +570,58 @@ namespace Pug.Application.Data
 					using( dataSession = applicationData.GetSession() )
 					{
 						result = function(dataSession, context);
+					}
+
+					tx.Complete();
+				}
+			}
+			catch (Exception exception)
+			{
+				successful = false;
+
+				if (onError != null)
+					onError(exception, context);
+
+				if (errorHandler == null)
+					throw;
+
+				errorHandler(exception, context);
+			}
+			finally
+			{
+				if (successful && onSuccess != null)
+					onSuccess(context);
+
+				if (onFinished != null)
+					onFinished(context);
+			}
+
+			return result;
+		}
+		
+		public static async Task<TResult> ExecuteAsync<TDataSession, TContext, TResult>(
+			this IApplicationData<TDataSession> applicationData,
+			Func<TDataSession, TContext, Task<TResult>> function,
+			TContext context,
+			TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+			Action<Exception, TContext> onError = null,
+			Action<Exception, TContext> errorHandler = null,
+			Action<TContext> onSuccess = null,
+			Action<TContext> onFinished = null
+		)
+			where TDataSession : class, IApplicationDataSession
+		{
+			TDataSession dataSession = null;
+			bool successful = true;
+			TResult result = default(TResult);
+
+			try
+			{
+				using (TransactionScope tx = new TransactionScope(transactionScopeOption))
+				{
+					using( dataSession = applicationData.GetSession() )
+					{
+						result = await function(dataSession, context).ConfigureAwait(false);
 					}
 
 					tx.Complete();
